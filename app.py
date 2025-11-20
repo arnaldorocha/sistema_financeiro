f38eaf5a79e044af86a1c5c08eb5018a60cb6425
ArnaldoRochaFilho <65460206+arnaldorocha@users.noreply.github.com>
2025-11-20 17:29:57 -0300

    Simplify app.run call in main execution

--- app.py
+++ app.py
@@ -625,5 +625,4 @@ def calendario():
 # Execução do Aplicativo
 # =====================================================
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
